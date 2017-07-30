85944c5d7a3fd4856402cfc2f9c4f3149c8fa647
MarthavBirapa <1503322H@student.tp.edu.sg>
2017-07-30 12:20:23 +0800

    Update LAB3BRemediation.sh

--- 1503322H/LAB3BRemediation.sh
+++ 1503322H/LAB3BRemediation.sh
@@ -193,54 +193,209 @@ cat /etc/passwd | awk -F : '{print $1, $3, $6}' | while read user uid dir; do
 			printf "\e[31mThe home directory ($dir) of user $user1 is owned by $owner.\e[0m\n"
 		else 
 			printf "\e[32mPass\e[0m\n"
-            userdel $user1
+            		userdel $user1
 		fi
 		done
 	fi
 done
 
 #7.18
-#Based on the results of the script, establish unique UIDs and review all files owned by the shared UID to determine which UID they are supposed to belong to.
+printf "\n \033[0;30m${bold}7.18 Check for Duplicate UIDs${normal} \n\n"
+printf "Checking for duplicate UIDs\n"
+#Get etc/passwd file
+cat /etc/passwd| cut -f3 -d":" | sort -n | uniq -c | while read x ; do
+[ -z "${x}" ] && break
+set - $x
+if [ $1 -gt 1 ]; then
+#Checks for duplicate UIDs
+	users=`/bin/gawk -F: '($3 == n) { print $1 }' n=$2 /etc/passwd| /usr/bin/xargs`
+	printf "\e[31mFail - Duplicate UID ($2)\e[0m\n"
+	printf "Please establish unique UIDs and review all files owned by the shared UID to determine which UID they are supposed to belong to."
+else
+	printf "\e[32mPass - UID ($2)\e[0m\n"
+fi
+done
 
 #7.19
-#Based on the results of the script, establish unique GIDs and review all files owned by the shared GID to determine which group they are supposed to belong to.
+printf "\n \033[0;30m${bold}7.19 Check for Duplicate GIDs${normal} \n\n"
+printf "Checking for duplicate GIDs\n"
+#Get etc/group file
+cat /etc/group | cut -f3 -d":" | sort -n | uniq -c | while read x ; do
+[ -z "${x}" ] && break
+set - $x
+if [ $1 -gt 1 ]; then
+#Checks for duplicate GIDs
+	grps=`/bin/gawk -F: '($3 == n) { print $1 }' n=$2 /etc/group | /usr/bin/xargs`
+	printf "\e[31mFail - Duplicate GID ($2)\e[0m\n"
+	printf "Please establish unique GIDs and review all files owned by the shared GID to determine which group they are supposed to belong to."
+else
+	printf "\e[32mPass - GID ($2)\e[0m\n"
+fi
+done
 
 #7.20
 #Based on the results of the above, change any UIDs that are in the reserved range to one that is in the user range. 
 #Review all files owned by the reserved UID to determine which UID they are supposed to belong to.
 
 #7.21
-#Based on the results of the script, establish unique user names for the users. 
-#File ownerships will automatically reflect the change as long as the users have unique UIDs.
+printf "\n \033[0;30m${bold}7.21 Check for Duplicate User Names${normal} \n\n"
+#Get etc/passwd file
+cat /etc/passwd | cut -f1 -d":" | /bin/sort -n | /usr/bin/uniq -c | while read x ; do
+[ -z "${x}" ] && break
+set - $x
+#Checks for duplicate user names
+if [ $1 -gt 1 ]; then
+uids=`/bin/gawk -F: '($1 == n) { print $3 }' n=$2 \/etc/passwd | xargs`
+printf "\e[31mFail - Duplicate User Name ($2)\e[0m\n"
+printf "Please establish unique user names for the users. "
+else
+printf "\e[32mPass - ($2)\e[0m\n"
+fi
+done
 
 #7.22
-#Based on the results of the script, establish unique names for the user groups. 
-#File group ownerships will automatically reflect the change as long as the groups have unique GIDs.
-
+printf "\n \033[0;30m${bold}7.22 Check for Duplicate Group Names${normal} \n\n"
+#Get etc/group file
+cat /etc/group | cut -f1 -d":" | /bin/sort -n | /usr/bin/uniq -c | while read x ; do
+[ -z "${x}" ] && break
+set - $x
+#Checks for duplicate group names
+if [ $1 -gt 1 ]; then
+gids=`/bin/gawk -F: '($1 == n) { print $3 }' n=$2 /etc/group | xargs`
+printf "\e[31mFail - Duplicate Group Name ($2)\e[0m\n"
+printf "Please establish unique names for the user groups."
+else 
+printf "\e[32mPass - ($2)\e[0m\n"
+fi
+done
 
 #7.23
 #Making global modifications to users' files without alerting the user community can result in unexpected outages and unhappy users. 
 #Therefore, it is recommended that a monitoring policy be established to report user .forward files and determine the action to be taken in accordance with site policy.
 
 #8.1
-touch /etc/motd
-echo "Authorized uses only. All activity may be \monitored and reported." > /etc/issue
-echo "Authorized uses only. All activity may be \monitored and reported." > /etc/issue.net
-chown root:root /etc/motd; chmod 644 /etc/motd
-chown root:root /etc/issue; chmod 644 /etc/issue
-chown root:root /etc/issue.net; chmod 644 /etc/issue.net
+printf "\n \033[0;30m${bold}8.1 Set Warning Banner for Standard Login Services${normal} \n\n"
+#Set counter to 0 and cut user and group of /etc/motd, /etc/issue and /etc/issue.net
+counter=0
+motdper=$(ls -l /etc/motd | cut -d " " -f 3 )
+motdper1=$(ls -l /etc/motd | cut -d " " -f 4 )
+issueper=$(ls -l /etc/issue | cut -d " " -f 3 )
+issueper1=$(ls -l /etc/issue | cut -d " " -f 4 )
+issuenetper=$(ls -l /etc/issue.net | cut -d " " -f 3 )
+issuenetper1=$(ls -l /etc/issue.net | cut -d " " -f 4 )
+#Checks if all are set as root 
+printf "Checking that /etc/motd /etc/issue /etc/issue.net have root as user and group:\n"
+if [ "$motdper" == "root" ] && [ "$motdper1" == "root" ] && [ "$issueper" == "root" ] && [ "$issueper1" == "root" ] && [ "$issuenetper" == "root" ] && [ "$issuenetper1" == "root" ]
+then 
+	printf "\033[33;32m PASS \n"
+else
+	counter=$((counter+1))
+	printf "\033[33;31m FAIL \n"
+
+fi
+echo  -en "\e[0m"
+#chmod of the 3 files
+chmodmotd=$( stat --format '%a' /etc/motd)
+chmodissue=$( stat --format '%a' /etc/issue)
+chmodissuenet=$( stat --format '%a' /etc/issue.net)
+#Checks if all files' chmod equals 644
+printf "Checking that /etc/motd /etc/issue /etc/issue.net have chmod of 644 :\n"
+if [ "$chmodmotd" -eq 644 ] && [ "$chmodissue" -eq 644 ] && [ "$chmodissuenet" -eq 644 ]
+then 
+	printf "\033[33;32m PASS \n"
+else	
+	counter=$((counter+1))
+	printf "\033[33;31m FAIL \n"
+fi
+echo  -en "\e[0m"
+#Checks if correct banner is set in issue and issue.net
+mp="Authorized uses only. All activity may be \ monitored and reported."
+catissue=$(cat /etc/issue)
+catissuenet=$(cat /etc/issue.net)
+printf "Checking that /etc/issue /etc/issue.net have proper motd :\n"
+if [ "$catissue" == "$mp" ] && [ "$catissuenet" == "$mp" ]
+then
+	printf "\033[33;32m PASS \n"
+else
+	counter=$((counter+1))
+	printf "\033[33;31m FAIL \n"
+fi
+echo  -en "\e[0m"
+#If counter more than 0 then fails
+if [ $counter -gt 0 ]; then
+	printf "\e[31mOverall Fail\e[0m\n"
+	touch /etc/motd
+	echo "Authorized uses only. All activity may be \monitored and reported." > /etc/issue
+	echo "Authorized uses only. All activity may be \monitored and reported." > /etc/issue.net
+	chown root:root /etc/motd; chmod 644 /etc/motd
+	chown root:root /etc/issue; chmod 644 /etc/issue
+	chown root:root /etc/issue.net; chmod 644 /etc/issue.net
+else
+	printf "\e[32mOverall Pass\e[0m\n"
+fi
+
 
 #8.2
 #Edit the /etc/motd, /etc/issue and /etc/issue.net files and remove any lines containing \m, \r, \s or \v.
-sed -i '/\m/ d' /etc/motd
-sed -i '/\r/ d' /etc/motd
-sed -i '/\s/ d' /etc/motd
-sed -i '/\v/ d' /etc/motd
-sed -i '/\m/ d' /etc/issue
-sed -i '/\r/ d' /etc/issue
-sed -i '/\s/ d' /etc/issue
-sed -i '/\v/ d' /etc/issue
-sed -i '/\m/ d' /etc/issue.net
-sed -i '/\r/ d' /etc/issue.net
-sed -i '/\s/ d' /etc/issue.net
-sed -i '/\v/ d' /etc/issue.net
+printf "\n \033[0;30m${bold}8.2 Remove OS information from Login Banners${normal} \n\n"
+#Greps if match 3 files
+issue=$(egrep '(\\v|\\r|\\m|\\s)' /etc/issue)
+motd=$(egrep '(\\v|\\r|\\m|\\s)' /etc/motd)
+issuenet=$(egrep '(\\v|\\r|\\m|\\s)' /etc/issue.net)
+#Set regular expression and counter
+regex='(\\v|\\r|\\m|\\s)'
+counter=0
+#echo $issue
+#Checks if match regex
+printf "Checking /etc/issue:\n"
+if [[ $issue =~ $regex ]]
+then 
+	counter=$((counter+1))
+	printf "\033[33;31m FAIL \n"
+else
+	printf "\033[33;32m PASS \n"
+fi
+echo  -en "\e[0m"
+#echo $motd
+#Checks if match regex
+printf "Checking /etc/motd:\n"
+if [[ $motd =~ $regex ]]
+then 
+	counter=$((counter+1))
+	printf "\033[33;31m FAIL \n"
+else
+	printf "\033[33;32m PASS \n"
+
+fi
+echo  -en "\e[0m"
+#echo $issuenet
+#Checks if match regex
+printf "Checking /etc/issue.net:\n"
+if [[ $issuenet =~ $regex ]]
+then 
+	counter=$((counter+1))
+	printf "\033[33;31m FAIL \n"
+else
+	printf "\033[33;32m PASS \n"
+fi
+echo  -en "\e[0m"
+#echo $counter
+#Checks if counter greater than 0
+if [ $counter -gt 0 ]; then
+	printf "\e[31mOverall Fail\e[0m\n"
+	sed -i '/\m/ d' /etc/motd
+	sed -i '/\r/ d' /etc/motd
+	sed -i '/\s/ d' /etc/motd
+	sed -i '/\v/ d' /etc/motd
+	sed -i '/\m/ d' /etc/issue
+	sed -i '/\r/ d' /etc/issue
+	sed -i '/\s/ d' /etc/issue
+	sed -i '/\v/ d' /etc/issue
+	sed -i '/\m/ d' /etc/issue.net
+	sed -i '/\r/ d' /etc/issue.net
+	sed -i '/\s/ d' /etc/issue.net
+	sed -i '/\v/ d' /etc/issue.net
+else
+	printf "\e[32mOverall Pass\e[0m\n"
+fi
+
